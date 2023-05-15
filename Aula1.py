0d0e6d30087dd9129715ae766eb67cf180559b79
Lucas pinheiro <lucaslogos@gmail.com>
2023-05-15 19:01:33 -0300

    update

--- Aula1.py
+++ Aula1.py
@@ -3,7 +3,7 @@ import numpy as np
 
 km = np.loadtxt('data/carros-km.txt')
 anos = np.loadtxt('data/carros-anos.txt', dtype=int)
+km_media_nan = km / (2019 - anos)
 
-km_media = km / (2019 - anos)
-km_media_nan = km_media[np.isfinite(km_media)]
-print(km_media_nan)
+km_media = km_media_nan[np.isfinite(km_media_nan)]
+print(km_media)
